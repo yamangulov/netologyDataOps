b2c788f0af2049b9614dd845d9f6c2bb3aef5aad
Andrey Yamangulov <yamangulov@gmail.com>
2022-05-06 11:40:04 +0300

    исправлены замечания преподавателя #2

--- spark/task2.py
+++ spark/task2.py
@@ -4,10 +4,10 @@ from pyspark.sql.functions import *
 spark = SparkSession.builder.appName("task2").master("local[2]").getOrCreate()
 spark.sql("set spark.sql.legacy.timeParserPolicy=LEGACY")
 
-df = spark.read.option('header', True).option('sep', ',').option('inferSchema', True).csv('owid-covid-data.csv').filter(col('iso_code').startswith('OWID') == False).withColumn('week_of_month', date_format(col('date'), 'W')).withColumn('month', month(col('date'))).withColumn('year', year(col('date')))
-march_last_week_number = df.filter((df.month == 3) & (df.year == 2021)).select(max('week_of_month').alias('value'))
-march_last_week_number = march_last_week_number.first()['value']
-df_result = df.filter((col('week_of_month') == march_last_week_number) & (col('month') == 3) & (col('year') == 2021)).groupby('location', 'date').max('new_cases').sort(desc('max(new_cases)')).limit(10)
+df = spark.read.option('header', True).option('sep', ',').option('inferSchema', True).csv('owid-covid-data.csv').filter(col('iso_code').startswith('OWID') == False).withColumn('day', dayofmonth(col('date'))).withColumn('month', month(col('date'))).withColumn('year', year(col('date')))
+
+df_result = df.filter(((col('day') == 29) | (col('day') == 30) | (col('day') == 31)) & (col('month') == 3) & (col('year') == 2021))
+df_result = df.join(df_result.groupby('location').agg(max('new_cases').alias('new_cases')), on='new_cases', how='leftsemi').select('date', 'location', 'new_cases').sort(desc('new_cases')).limit(10)
 df_result.write.csv('tmp/task2', header=True)
 
 print('task2 is finished')
--- spark/task3.py
+++ spark/task3.py
@@ -5,12 +5,9 @@ from pyspark.sql.window import Window
 spark = SparkSession.builder.appName("task3").master("local[2]").getOrCreate()
 spark.sql("set spark.sql.legacy.timeParserPolicy=LEGACY")
 
-df = spark.read.option('header', True).option('sep', ',').option('inferSchema', True).csv('owid-covid-data.csv').withColumn('week_of_month', date_format(col('date'), 'W')).withColumn('month', month(col('date'))).withColumn('year', year(col('date')))
+df = spark.read.option('header', True).option('sep', ',').option('inferSchema', True).csv('owid-covid-data.csv').withColumn('day', dayofmonth(col('date'))).withColumn('month', month(col('date'))).withColumn('year', year(col('date')))
 
-march_last_week_number = df.filter((df.month == 3) & (df.year == 2021)).select(max('week_of_month').alias('value'))
-march_last_week_number = march_last_week_number.first()['value']
-
-df_filtered = df.filter((col('week_of_month') == march_last_week_number) & (col('month') == 3) & (col('year') == 2021) & (col('location') == 'Russia'))
+df_filtered = df.filter(((col('day') == 29) | (col('day') == 30) | (col('day') == 31)) & (col('month') == 3) & (col('year') == 2021) & (col('location') == 'Russia'))
 df_window = Window.partitionBy().orderBy('date')
 df_result = df_filtered.withColumn('prev_new_cases', lag(df_filtered.new_cases).over(df_window))
 df_result = df_result.withColumn('delta', df_result.new_cases - df_result.prev_new_cases).select('date', 'prev_new_cases', 'new_cases', 'delta')
